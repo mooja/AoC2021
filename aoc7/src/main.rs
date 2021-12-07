0a10d057620bfc1c2eeb02e7d1623eee73582470
Max Shkurygin <max.atreides@gmail.com>
2021-12-07 08:43:02 -0500

    Day 7

--- aoc7/src/main.rs
+++ aoc7/src/main.rs
@@ -1,3 +1,43 @@
+use counter::Counter;
+
+fn solution(input: &str, part1: bool) {
+    let counts = parse_input(input);
+    let mut distances = vec![];
+
+    for &target in counts.keys() {
+        let mut total_distance = 0;
+
+        for (&pos, &ncrabs) in counts.into_iter() {
+            let delta = (target as i32 - pos as i32).abs() as u32;
+            let fuel_cost = if part1 {
+                delta
+            } else {
+                ((delta as f32 / 2.0) * (delta + 1) as f32) as u32
+            };
+
+            total_distance += ncrabs as u32 * fuel_cost;
+        }
+
+        distances.push(total_distance);
+    }
+
+    println!(
+        "Part {}: {}",
+        if part1 { '1' } else { '2' },
+        distances.iter().min().unwrap()
+    );
+}
+
+fn parse_input(input: &str) -> Counter<u32> {
+    input
+        .trim()
+        .split(",")
+        .map(|s| s.parse::<u32>().unwrap())
+        .collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = &std::fs::read_to_string("aoc7.txt").unwrap();
+    solution(input, true);
+    solution(input, false);
 }
