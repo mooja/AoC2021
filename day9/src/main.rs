0464ff822f836a9d8c5ff421331d0e457989378a
Max Shkurygin <max.atreides@gmail.com>
2021-12-09 11:26:21 -0500

    Cleanup.

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,10 +1,14 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 type Point = (usize, usize);
 
-struct DepthGrid(Vec<Vec<u32>>);
+struct DepthGrid(Vec<Vec<u8>>);
 
 impl DepthGrid {
+    fn get(&self, p: Point) -> u8 {
+        self.0[p.0][p.1]
+    }
+
     fn neighbors(&self, row: usize, col: usize) -> impl Iterator<Item = (usize, usize)> + '_ {
         let (row, col) = (row as i32, col as i32);
         vec![
@@ -22,8 +26,8 @@ impl DepthGrid {
     }
 
     fn is_lowpoint(&self, row: usize, col: usize) -> bool {
-        let p = self.0[row as usize][col as usize];
-        self.neighbors(row, col).all(|(r, c)| p < self.0[r][c])
+        let p = self.get((row, col));
+        self.neighbors(row, col).all(|(r, c)| p < self.get((r, c)))
     }
 
     fn lowpoints(&self) -> impl Iterator<Item = (usize, usize)> + '_ {
@@ -32,47 +36,41 @@ impl DepthGrid {
             .flatten()
             .filter(|&(r, c)| self.is_lowpoint(r, c))
     }
-
-    fn lowpoints_sum(&self) -> u32 {
-        let mut acc = 0;
-        for row_idx in 0..self.0.len() {
-            for col_idx in 0..self.0[0].len() {
-                if self.is_lowpoint(row_idx, col_idx) {
-                    acc += 1 + self.0[row_idx][col_idx];
-                }
-            }
-        }
-
-        acc
-    }
 }
 
 fn main() {
     let input = std::fs::read_to_string("aoc9.txt").unwrap();
     let depth_grid = parse_input(&input);
-    println!("Part 1: {}", depth_grid.lowpoints_sum());
+    println!(
+        "Part 1: {}",
+        depth_grid
+            .lowpoints()
+            .map(|p| depth_grid.get(p))
+            .sum::<u8>()
+    );
 
-    let mut basins: HashMap<Point, Vec<Point>> =
-        depth_grid.lowpoints().map(|lp| (lp, vec![lp])).collect();
+    let mut basins: Vec<HashSet<Point>> = depth_grid
+        .lowpoints()
+        .map(|p| vec![p].into_iter().collect())
+        .collect();
 
-    for (&initial_p, seen_basin_ps) in basins.iter_mut() {
-        let mut queue = vec![initial_p];
+    for b in basins.iter_mut() {
+        let mut queue = vec![b.clone().into_iter().next().unwrap()];
         while let Some(p) = queue.pop() {
-            let neighbors = depth_grid
+            let neighbors: Vec<Point> = depth_grid
                 .neighbors(p.0, p.1)
-                .filter(|&(r, c)| depth_grid.0[r][c] != 9)
-                .filter(|&(r, c)| !seen_basin_ps.contains(&(r, c)))
-                .collect::<Vec<_>>();
+                .filter(|&(r, c)| depth_grid.get((r, c)) != 9)
+                .filter(|&(r, c)| !b.contains(&(r, c)))
+                .collect();
 
             for n in neighbors {
                 queue.push(n);
-                seen_basin_ps.push(n);
+                b.insert(n);
             }
         }
     }
 
-    let mut basin_sizes = basins.iter().map(|(_, ps)| ps.len()).collect::<Vec<_>>();
-
+    let mut basin_sizes = basins.into_iter().map(|ps| ps.len()).collect::<Vec<_>>();
     basin_sizes.sort_by(|a, b| b.cmp(a));
     println!(
         "Part 2: {}",
@@ -87,7 +85,7 @@ fn parse_input(input: &str) -> DepthGrid {
             .split("\n")
             .map(|line| {
                 line.chars()
-                    .map(|ch| (ch as u8 - '0' as u8) as u32)
+                    .map(|ch| ch as u8 - '0' as u8)
                     .collect::<Vec<_>>()
             })
             .collect::<Vec<_>>(),
