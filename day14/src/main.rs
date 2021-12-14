179b42cd3df338d69e76afad57385061a80afe5d
Max Shkurygin <max.atreides@gmail.com>
2021-12-14 10:27:23 -0500

    Cleanup.

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,25 +1,23 @@
 use counter::Counter;
-
 use std::collections::HashMap;
 
-type State = Counter<[char; 2], u128>;
-type Rules = HashMap<[char; 2], ([char; 2], [char; 2])>;
+type Pair = [char; 2];
+type State = Counter<Pair, u128>;
+type Rules = HashMap<Pair, (Pair, Pair)>;
 
 fn main() {
     let input = std::fs::read_to_string("aoc14.txt").unwrap();
     let (mut state, rules) = parse_input(&input);
 
     for _ in 0..40 {
-        let mut new_state = state.clone();
+        let mut new_state = Counter::new();
 
-        for (old_pair, count) in &state {
+        for (old_pair, &count) in &state {
             let new_pairs = rules.get(old_pair).unwrap();
 
             for new_pair in &[new_pairs.0, new_pairs.1] {
-                new_state[new_pair] += *count as u128;
+                new_state[new_pair] += count as u128;
             }
-
-            new_state[old_pair] -= count;
         }
 
         state = new_state;
@@ -27,24 +25,23 @@ fn main() {
 
     let mut el_counts = state
         .iter()
-        .map(|(keys, _)| keys.iter())
+        .map(|(pair, _)| pair.iter())
         .flatten()
-        .map(|el| (*el, 0))
+        .map(|&el| (el, 0))
         .collect::<Counter<char, u128>>();
 
-    let elem_names = el_counts.keys().copied().collect::<Vec<_>>();
-    for &elem in &elem_names {
+    for (&elem, el_count) in el_counts.iter_mut() {
         let ls_count = state
             .iter()
-            .filter_map(|(&pair, count)| (pair[0] == elem).then(|| *count as u128))
+            .filter_map(|(&pair, &count)| (pair[0] == elem).then(|| count as u128))
             .sum::<u128>();
 
         let rs_count = state
             .iter()
-            .filter_map(|(&pair, count)| (pair[1] == elem).then(|| *count as u128))
+            .filter_map(|(&pair, &count)| (pair[1] == elem).then(|| count as u128))
             .sum::<u128>();
 
-        el_counts[&elem] = ls_count.max(rs_count);
+        *el_count = ls_count.max(rs_count);
     }
 
     let ordered = el_counts.most_common_ordered();
@@ -55,32 +52,27 @@ fn main() {
 }
 
 fn parse_input(input: &str) -> (State, Rules) {
-    let mut iter = input.trim().split("\n\n");
-    let state: State = iter
-        .next()
-        .unwrap()
-        .as_bytes()
-        .iter()
-        .map(|&b| b as char)
-        .collect::<Vec<_>>()
-        .windows(2)
-        .into_iter()
-        .map(|arr| [arr[0], arr[1]])
+    let (state, rules) = input.trim().split_once("\n\n").unwrap();
+
+    let init_state: State = (0..state.len() - 2)
+        .map(|idx| [state.as_bytes()[idx] as char, state.as_bytes()[idx+1] as char])
         .collect::<Counter<[char; 2], u128>>();
-    let rules: HashMap<[char; 2], ([char; 2], [char; 2])> = iter
-        .next()
-        .unwrap()
+
+    let rules: Rules = rules
         .trim()
         .split('\n')
-        .map(|l| l.split_once(" -> ").unwrap())
-        .map(|(l, r)| {
-            let l = l.as_bytes();
-            let l: [char; 2] = [l[0] as char, l[1] as char];
-            let r = r.as_bytes()[0];
-            let r = ([l[0] as char, r as char], [r as char, l[1] as char]);
-            (l, r)
+        .map(|line| {
+            let (k, v) = line.split_once(" -> ").unwrap();
+
+            let k = k.as_bytes();
+            let k: [char; 2] = [k[0] as char, k[1] as char];
+
+            let v = v.as_bytes()[0];
+            let v = ([k[0] as char, v as char], [v as char, k[1] as char]);
+
+            (k, v)
         })
         .collect();
 
-    (state, rules)
+    (init_state, rules)
 }
